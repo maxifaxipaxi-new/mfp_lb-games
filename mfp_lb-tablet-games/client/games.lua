15b0fe2c53296a6fa966d4e79764747ddee2f9cd
Maximus7474 <the.maximus.7474@gmail.com>
2025-06-02 11:38:38 +0200

    feat(client): list all games a lua table for tablet

---
+++ mfp_lb-tablet-games/client/games.lua
@@ -0,0 +1,60 @@
+---@class GameData
+---@field title string
+---@field description string
+---@field gameUrl string file path from the games/ directory (including games/)
+---@field icon string file path for the game icon
+---@field banner string unused at this time, only referenced
+---@field size? number simulated size of the app for the phone
+---@field images? string[] array of file paths for app pictures, used in app store
+
+Games = {
+    {
+        title = "1024",
+        description = "Classic 1024 game.",
+        gameUrl = "games/1024/game/game.html",
+        icon = "games/1024/assets/icon.png",
+        banner = "games/1024/assets/banner.jpg"
+    },
+    {
+        title = "CLASSIC Pac-Man",
+        description = "Classic Pac-Man game.",
+        gameUrl = "games/pacman/app/index.html",
+        icon = "games/pacman/icon.webp",
+        banner = "games/pacman/banner.jpg"
+    },
+    {
+        title = "Tic Tac Toe",
+        description = "Don't win a beer!",
+        gameUrl = "games/tictactoe/app/index.html",
+        icon = "games/tictactoe/icon.png",
+        banner = "games/tictactoe/banner.png"
+    },
+    {
+        title = "CLASSIC Snake",
+        description = "Classic Snake game.",
+        gameUrl = "games/snake/app/index.html",
+        icon = "games/snake/snake.png",
+        banner = "games/snake/banner.webp"
+    },
+    {
+        title = "CLASSIC Teetris",
+        description = "Classic Snake game.",
+        gameUrl = "games/teetris/app/index.html",
+        icon = "games/teetris/icon.png",
+        banner = "games/teetris/banner.jpg"
+    },
+    {
+        title = "CLASSIC Alien Invasion",
+        description = "Classic Alien Invasion game.",
+        gameUrl = "games/alieninvasion/game/index.html",
+        icon = "games/alieninvasion/assets/icon.jpg",
+        banner = "games/alieninvasion/assets/banner.png"
+    },
+    {
+        title = "Flappy Börd",
+        description = "Classic Flappy Börd game.",
+        gameUrl = "games/flappybird/index.html",
+        icon = "games/flappybird/assets/icon.png",
+        banner = "games/flappybird/assets/banner.webp"
+    }
+} --[[ @as GameData[] ]]
